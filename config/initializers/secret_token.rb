21e9ca86c9708fcc73ce5f2bb6fe4b5aa553d7ce
Marcio Lima <mdlima.github@mailnull.com>
2012-05-15 11:34:22 -0300

    Setting up a rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TshirtidTeaser::Application.config.secret_token = '4c318f07059369ec51178b6c3f87b6f4ace1a219c4b6b2a9794b38d02bdea440ee7d9c65c989098c0f85dec35c52c502e423a71bab32882206ac7c2130d6a272'
