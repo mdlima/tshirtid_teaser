6478bce49714f945fb40b7b0220fca79102bb552
Marcio Lima <mdlima.github@mailnull.com>
2012-05-16 17:00:32 -0300

    Haml config for HTML5.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TshirtidTeaser::Application.initialize!
+
+# Haml options
+Haml::Template.options[:format] = :html5
