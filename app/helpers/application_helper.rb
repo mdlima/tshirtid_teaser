a4234cf6e61b4cc59c74bd42d8132e068acb9fa1
Marcio Lima <mdlima.github@mailnull.com>
2012-05-16 16:58:56 -0300

    Adds helper method for preparing flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+
+  
+  def prepare_flash_message(flash_type, flash_value)
+    prepared_message = { :class => "alert-info", :heading => "", :message => ""}
+    
+    case flash_type
+      when :alert
+        prepared_message[:class] = "alert-warning"
+      when :error
+        prepared_message[:class] = "alert-error"
+      when :notice
+        prepared_message[:class] = "alert-info"
+      when :success
+        prepared_message[:class] = "alert-success"
+      # Defaults to initialized value above
+      # else
+      #   type.to_s
+    end
+
+    if flash_value.class == Hash
+      prepared_message[:heading] = flash_value[:heading]
+      prepared_message[:message] = flash_value[:message]
+    else
+      prepared_message[:message] = flash_value
+    end
+    
+    return prepared_message
+    
+  end
+  
 end
