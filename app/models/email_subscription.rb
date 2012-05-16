c4d495b49c8951fdb8b884c96eba938973d880e0
Marcio Lima <mdlima.github@mailnull.com>
2012-05-16 17:00:57 -0300

    Annotates email_subscriptions model.

--- app/models/email_subscription.rb
+++ app/models/email_subscription.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: email_subscriptions
+#
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  opt_in          :boolean
+#  opt_in_campaign :string(255)
+#  ip              :string(255)
+#  last_opt_in     :datetime
+#  last_opt_out    :datetime
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#
+
 class EmailSubscription < ActiveRecord::Base
   validates_presence_of :email, :message => 'Por favor, preencha o seu email.'
   validates_uniqueness_of :email, :message => 'Seu email já está cadastrado, obrigado.'
