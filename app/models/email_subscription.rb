6e4cf302b87aebd64e30abffca7a33c9c03759ac
Marcio Lima <mdlima.github@mailnull.com>
2012-05-17 16:06:03 -0300

    Uses HTML encoding for extended characters on model validations.

--- app/models/email_subscription.rb
+++ app/models/email_subscription.rb
@@ -16,10 +16,10 @@
 
 class EmailSubscription < ActiveRecord::Base
   validates_presence_of :email, :message => 'Por favor, preencha o seu email.'
-  validates_uniqueness_of :email, :message => 'Seu email já está cadastrado, obrigado.'
+  validates_uniqueness_of :email, :message => 'Seu email j&aacute; est&aacute; cadastrado, obrigado.'
   validates_format_of :email,
     :with => /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/i,
-    :message => 'Este email parece ser inválido.'
+    :message => 'Este email parece ser inv&aacute;lido.'
   
   attr_accessible :email, :ip, :last_opt_in, :last_opt_out, :name, :opt_in, :opt_in_campaign
   
