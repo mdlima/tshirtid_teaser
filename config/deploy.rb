40a58951f3ab8e671581e0439f504ea9cd178282
Marcio Lima <mdlima.github@mailnull.com>
2012-06-20 19:04:38 -0300

    Make deployment to dev environment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
 require "bundler/capistrano"
 
+# load 'deploy/assets'
+
 # ==============================================================
 # SET's
 # ==============================================================
@@ -7,7 +9,7 @@ require "bundler/capistrano"
 default_run_options[:pty] = true 
 
 set :user, "boscolotshirt"
-set :domain, "boscolotshirt.com.br"
+set :domain, "ftp.boscolotshirt.com.br"
 set :application, "dev_tshirt_id"
 # set :db_name, "boscolotshirt2"
 
@@ -25,7 +27,7 @@ set :git_shallow_clone, 1
 set :remote, user
 set :scm_verbose, true
 set :copy_cache, true 
-set :keep_releases, 3 # mantem 3 versoes, posso fazer ate 3 rollbacks de versao
+set :keep_releases, 5 # mantem 5 versoes
 
 set :build_nokogiri, "--with-xslt-dir=/home/storage/c/9a/91/boscolotshirt/local --with-xml2-include=/home/storage/c/9a/91/boscolotshirt/local/include/libxml2 --with-xml2-lib=/home/storage/c/9a/91/boscolotshirt/local/lib"
 
@@ -41,7 +43,9 @@ server domain, :app, :web, :db, :primary => true
 # ==============================================================
  
 after "deploy:update_code", :roles => [:web, :db, :app] do
-  run "chmod 755 #{release_path}/public -R" 
+  deploy.copy_database_config
+  run "chmod 755 #{release_path}/public -R"
+  deploy.deploy_assets
 end
 
 
@@ -51,6 +55,22 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  
+  desc "copy database.yml into project"
+  task :copy_database_config do
+    # production_db_config = "/path_to_config/#{application}.yml"
+    run "cp /home/#{user}/rails_projects/#{application}/database.yml #{current_release}/config/database.yml"
+    puts "replaced database.yml with live copy"
+  end
+  
+  desc "uploads precompiled  assets to production"
+  task :deploy_assets, :except => { :no_release => true } do
+     run_locally("rake assets:clean && rake assets:precompile")
+     # puts "uploading to #{release_path}/public/assets"
+     top.upload("public/assets", "#{release_path}/public/assets", :via => :scp, :recursive => true)
+     # top.upload("public/assets", "/home/boscolotshirt/rails_projects/dev_tshirt_id/releases/20120620212654/public/assets", :via => :scp, :recursive => true)
+     run_locally("rake assets:clean")
   end
 end
 
